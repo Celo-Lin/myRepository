4bc8d27f015a1845e7d45bbbfefce2cee527f543
linchaolong <L18938422807@163.com>
2024-12-26 22:12:09 +0800

    Revert "新增了一个02"
    This reverts commit 14ff01664788e410a2aa24060a800cbe8616bd1e.

--- yl-adx-manage/src/main/java/com/juan/adx/manage/action/adx/dsp/DspBudgetPlanAction.java
+++ yl-adx-manage/src/main/java/com/juan/adx/manage/action/adx/dsp/DspBudgetPlanAction.java
@@ -23,7 +23,6 @@ import javax.annotation.Resource;
 @RequestMapping("/adx/dsp/plan")
 public class DspBudgetPlanAction {
     //新增了一个01
-    //新增了一个02
     @Resource
     private DspBudgetPlanService dspBudgetPlanService;
 
